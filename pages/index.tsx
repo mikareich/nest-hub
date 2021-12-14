f5bea5c39f586e5467d33bf4b627a82d08e8af9e
Mika Reich <mikareich0@gmail.com>
2021-12-14 22:15:26 +0100

    add clock

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import View from "../components/View";
 
 const Index: NextPage = function () {
+  const [currentTime, setCurrentTime] = useState(Date.now());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(Date.now());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   const imageGallery = [
     "https://images.pexels.com/photos/1152359/pexels-photo-1152359.jpeg?auto=compress",
     "https://images.pexels.com/photos/1170572/pexels-photo-1170572.jpeg?auto=compress",
@@ -10,8 +20,23 @@ const Index: NextPage = function () {
   ];
 
   return (
-    <View title="Nest Hub" imageGallery={imageGallery}>
-      Hey
+    <View title="Nest Hub" imageGallery={imageGallery} withBanner>
+      <div className="text-white absolute bottom-0 left-0">
+        <div className="text-7xl">
+          {new Date(currentTime).toLocaleTimeString("de-DE", {
+            hour: "2-digit",
+            minute: "2-digit",
+          })}
+        </div>
+        <div className="text-lg">
+          {new Date(currentTime).toLocaleDateString("de-DE", {
+            weekday: "short",
+            day: "numeric",
+            month: "numeric",
+            year: "numeric",
+          })}
+        </div>
+      </div>
     </View>
   );
 };
